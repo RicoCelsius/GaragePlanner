efd6dd716bf1f1db4978fdbb9f490b40498788c7
Rico <Rico.aarntzen@gmail.com>
2023-02-09 04:38:19 -0800

    Add project files.

---
+++ GaragePlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GaragePlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
