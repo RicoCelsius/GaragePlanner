188af236d0425d6170b659ecdb74a084de4d1512
Rico <Rico.aarntzen@gmail.com>
2023-03-29 06:50:43 -0700

    working on Customer DAL and Dbconnection

---
+++ DAL/DALCustomer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class DALCustomer
+    {
+
+
+        public void insertCustomer(string firstName, string lastName, string address, string email, string password)
+        {
+            string query = "INSERT INTO Customers (FirstName, LastName, Address, Email, Password) " +
+                           "VALUES (@FirstName, @LastName, @Address, @Email, @Password)";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+            new SqlParameter("@FirstName", SqlDbType.VarChar, 50) { Value = firstName },
+            new SqlParameter("@LastName", SqlDbType.VarChar, 50) { Value = lastName },
+            new SqlParameter("@Address", SqlDbType.VarChar, 100) { Value = address },
+            new SqlParameter("@Email", SqlDbType.VarChar, 50) { Value = email },
+            new SqlParameter("@Password", SqlDbType.VarChar, 50) { Value = password }
+            };
+
+            DbConnection connection = new DbConnection();
+            connection.ExecuteQuery(query, parameters);
+        }
+
+    }
+}
---
+++ DAL/DbConnection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DbConnection : IDbConnection
+    {
+        private string _connectionString = "Server = 127.0.0.1; Database=garageplanner;Uid=root;Pwd=;";
+        private SqlConnection _sqlConnection;
+        private bool isConnected;
+
+        public DbConnection()
+        {
+            _sqlConnection = new SqlConnection(_connectionString);
+            isConnected = false;
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                if (isConnected == false)
+                {
+                    _sqlConnection.Open();
+                    isConnected = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to connect to the database.", ex);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                if (isConnected == true)
+                {
+                    _sqlConnection.Close();
+                    isConnected = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to disconnect from the database.", ex);
+            }
+        }
+
+        public System.Data.DataTable ExecuteQuery(string query, SqlParameter[] parameters)
+        {
+            Connect();
+            try
+            {
+                SqlCommand command = new SqlCommand(query, _sqlConnection);
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error executing query.", ex);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+    }
+}
